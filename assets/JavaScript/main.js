baea32a6ff1edecf83470bdd08466f6caacdaaaf
CatalinBarbu <121017880+CatalinBarbu@users.noreply.github.com>
2023-01-03 15:20:46 +0100

    First commit

---
+++ assets/JavaScript/main.js
@@ -0,0 +1,15 @@
+/* window.alert("Hallo Welt");
+window.prompt("Bitte geben Sie Ihren Namen ein.");
+window.confirm("Stimmen Sie meiner Meinung zu?"); */
+/* window.prompt("Bitte geben Sie Ihren Namen ein", "Cancel"); */
+/* let alter = prompt("35");
+console.log(alter);
+
+let b = 5;
+let a = b * 5 - 3;
+alert(a); */
+
+/* let mann = "Robert Wadlow ist der größte Mann der Welt : ";
+let grosse = "2,72m";
+alert(mann + grosse);
+ */
